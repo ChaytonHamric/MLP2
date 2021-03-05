c86c73fc77cb675bd7c8c7e5da603f073dbc6011
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-05 17:27:19 -0500

    Trimmed Data and graphed it on a correlation scatter plot

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('data.csv', 
-    header=None, 
-    names=[
-        'ROA(C) before interest and depreciation before interest',
-        'ROA(A) before interest and % after tax',
-        'ROA(B) before interest and depreciation after tax',
-        'Operating Gross Margin',
-        'Realized Sales Gross Margin',
-        'Operating Profit Rate',
-        'Pre-tax net Interest Rate',
-        'After-tax net Interest Rate',
-        'Non-industry income and expenditure/revenue',
-        'Continuous interest rate (after tax)'
-        'Operating Expense Rate'
-        ])
-print(data)
+Data = pd.read_csv('data.csv')
+
+print(Data)
+
+# plt.plot(Data["Operating Profit Rate"], Data["Bankrupt?"], 'ro')
+# plt.ylabel('Operating Expense Rate')
+# plt.xlabel('Bankrupt Status')
+# plt.show()
+
+Data.rename(columns={'Bankrupt?': 'A',
+    'ROA(C) before interest and depreciation before interest': 'B',
+    'ROA(A) before interest and % after tax': 'C',
+    'ROA(B) before interest and depreciation after tax': 'D',
+    'Operating Gross Margin': 'E',
+    'Realized Sales Gross Margin': 'F',
+    'Operating Profit Rate': 'G',
+    'Pre-tax net Interest Rate': 'H',
+    'After-tax net Interest Rate': 'I',
+    'Non-industry income and expenditure/revenue': 'J',
+    'Continuous interest rate (after tax)': 'K',
+    'Operating Expense Rate': 'L'}, inplace=True)
+
+
+plt.style.use('dark_background')
+pd.plotting.scatter_matrix(Data)
+plt.show()
 
