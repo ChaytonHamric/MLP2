2fa5453c21751119feefbba61da268fa896e97d7
Chayton Hamric <ChaytonHamric@gmail.com>
2021-03-06 16:20:16 -0500

    Created confusion matrix for KNN, Log Reg, SVM

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.preprocessing import scale
-
+from sklearn.metrics import plot_confusion_matrix as pcm
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
 
 Data = pd.read_csv('data.csv')
-
 # print(Data)
 
 Data.rename(columns={'Bankrupt?': 'A',
@@ -25,9 +26,9 @@ Data.rename(columns={'Bankrupt?': 'A',
 
 plt.style.use('dark_background')
 pd.plotting.scatter_matrix(Data)
-# plt.show()
+#plt.show()
 
-X = Data.iloc[:, [1,3,11]]
+X = Data.iloc[:, [1-11]]
 x = scale(X)
 Y = Data["A"]
 X_train, X_test, Y_train, Y_test = train_test_split(x, Y, test_size=0.25, random_state=42)
@@ -43,10 +44,28 @@ print("Training set score: {:.2f}".format(lr.score(X_train, Y_train)))
 print("Test set score: {:.2f}".format(lr.score(X_test, Y_test)))
 
 # Use the trained logistic repression model to predict a new, previously unseen object
-ROAC = 0.3909228294
-ROAB = 0.4361582526
-OER = 0.0001157521304
-Bankrupt_prediction = lr.predict([[ROAC, ROAB, OER]])
-Bankrupt_probability = lr.predict_proba([[ROAC, ROAB, OER]])
-print("pass: {}".format(Bankrupt_prediction[0]))
-print("fail/pass probability: {}".format(Bankrupt_probability[0]))
+# ROAC = 0.3909228294
+# ROAB = 0.4361582526
+# OER = 0.0001157521304
+# Bankrupt_prediction = lr.predict([[ROAC, ROAB, OER]])
+# Bankrupt_probability = lr.predict_proba([[ROAC, ROAB, OER]])
+# print("pass: {}".format(Bankrupt_prediction[0]))
+# print("fail/pass probability: {}".format(Bankrupt_probability[0]))
+
+# KNN Confusion Matrix
+knn = KNeighborsClassifier(n_neighbors=15, metric='minkowski', p=1, weights='distance')
+knn.fit(X_train, Y_train)
+pcm(knn, X_test, Y_test, normalize='true', display_labels=['Not Bankrupt', 'Bankrupt'])
+
+
+# LR Confusion Matrix
+lrc = LogisticRegression(solver='liblinear', random_state=0)
+lrc.fit(X_train, Y_train)
+pcm(lrc, X_test, Y_test, normalize='true', display_labels=['Not Bankrupt', 'Bankrupt'])
+
+
+# SVM Confusion Matrix
+svc = SVC(C=10, degree=1, kernel='poly')
+svc.fit(X_train, Y_train)
+pcm(svc, X_test, Y_test, normalize='true', display_labels=['Not Bankrupt', 'Bankrupt'])
+plt.show()
